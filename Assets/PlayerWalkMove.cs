ef681832395be4c828088d87bb2a25a71e1446bc
Ethan Zarov <etz211@nyu.edu>
2019-11-05 17:15:35 -0500

    Added player movement scripts and basic camera following. + Basic 3D Character Model

---
+++ Assets/PlayerWalkMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWalkMove : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed;
+
+    [Header("Player Rotation")]
+    Quaternion suggestedRotation;
+    [Range(0f,1f)]
+    public float rotationSnapSpeed; 
+ 
+    void FixedUpdate()
+    {
+        Vector3 pos = PlayerManager.position;
+        Vector3 input = new Vector3((Input.GetAxis("Horizontal")), 0, (Input.GetAxis("Vertical")));
+        if (input.magnitude < .02f)
+        {
+            input = Vector3.zero;
+        }
+
+        Vector3 newPos = pos + (input * moveSpeed * Time.fixedDeltaTime);
+        PlayerManager.main.rb.MovePosition(newPos);
+
+        //Rotation Adjust
+        if (input.magnitude > .2f)
+        {
+            Quaternion suggestedRotation = Quaternion.LookRotation((newPos - pos).normalized);
+            Quaternion newRotation = Quaternion.Slerp(transform.rotation, suggestedRotation, rotationSnapSpeed);
+            PlayerManager.main.rb.MoveRotation(newRotation);
+        }
+
+        PlayerManager.main.rb.MovePosition(new Vector3(PlayerManager.main.rb.position.x, 0, PlayerManager.main.rb.position.z));
+
+
+
+    }
+}
