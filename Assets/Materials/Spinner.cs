9ea6d973b262515e74946783796c3287b1aa4c33
dynopa <dl3649@nyu.edu>
2019-11-27 04:51:35 -0500

    Added 3D Models for: Steetlamp, Policecar, Trafficlights, Simple Box/Crate, Pistol.

---
+++ Assets/Materials/Spinner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    //Add this to an object to make it spin (in the z-axis)
+
+    public float spinSpeedz;
+    public float spinSpeedy;
+    public float spinSpeedx;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(Time.deltaTime* spinSpeedx, Time.deltaTime * spinSpeedy, Time.deltaTime * spinSpeedz);
+    }
+}
