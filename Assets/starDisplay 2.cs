b04a7a14247658cde38f2e634e82225e42233a1a
Jordan Grayson <jtg400@nyu.edu>
2019-12-08 11:02:35 -0500

    Dropped in Ethan's star display script into the main scene

---
+++ Assets/starDisplay 2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class starDisplay : MonoBehaviour
+{
+    public int starNumber;
+    public Image starImage;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (NpcCopManager.starLevel >= starNumber)
+        {
+            starImage.enabled = true;
+        }
+        else starImage.enabled = false;
+    }
+}
