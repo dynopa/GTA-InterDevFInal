f72bd01fcc5e7794993db81df6aeb6df9ec061ce
dynopa <dl3649@nyu.edu>
2019-12-10 21:37:32 -0500

    Ending Scene Props, Basic Buttons for Start and Main Menu.

---
+++ Assets/StartScene/MainMenuButton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButton : MonoBehaviour {
+	public Button StartButton2;
+
+	void Start () {
+		Button btn2 = StartButton2.GetComponent<Button>();
+		btn2.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick(){
+		SceneManager.LoadScene(1);
+	}
+}
