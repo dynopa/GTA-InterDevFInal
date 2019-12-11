4e7f0c0d58b1addc3ba0782f58f115a68cec7cc7
Jordan Grayson <jtg400@nyu.edu>
2019-12-10 23:05:33 -0500

    Implemented a rough player death script. Scene dressed the middle piece.

---
+++ Assets/PlayerDeath.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+
+    [SerializeField] int health;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set up the health of this NPC
+        //health = this.GetComponent<NpcCivPersonalityManager>().personality.health;
+
+    }
+
+
+    /// <summary>
+    /// Reduces the health of the NPC for when they get shot. Takes in the damage dealt. After damage is dealt, checks if the NPC dies. If they are still alive, change their emotion to scared.
+    /// </summary>
+    /// <param name="damage">Damage.</param>
+    public void ReduceHealth(int damage)
+    {
+        health -= damage;
+        CheckForDeath();
+
+    }
+
+    /// <summary>
+    /// Checks for the death of the NPC.
+    /// </summary>
+    private bool CheckForDeath()
+    {
+        if (health <= 0)
+        {
+            //NpcCivManager.Instance.RemoveNpc(this.gameObject);
+            this.gameObject.GetComponent<PlayerWalkMove>().enabled = false;
+            this.gameObject.GetComponent<PlayerCarMove>().enabled = false;
+            this.gameObject.transform.Translate(new Vector3(0, -1, 0));
+            this.gameObject.transform.Rotate(new Vector3(70, 20, 0));
+            //ScoreManager.Instance.IncreaseScore(10);
+            //NpcCopManager.Instance.RemoveNpc(this.gameObject);
+            //NpcCopManager.Instance.CopDeath();
+            Invoke("StopForces", .5f);
+            return true;
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// Stops the forces acting on the NPC.
+    /// </summary>
+    private void StopForces()
+    {
+        this.gameObject.GetComponent<Collider>().enabled = false;
+        this.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+    }
+}
