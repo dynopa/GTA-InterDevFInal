e229317f01d06595dbcb695fe94a7b7040ae893a
dynopa <dl3649@nyu.edu>
2019-12-11 20:22:07 -0500

    Score Text UI.

--- Assets/StartScene/GameOver.cs
+++ Assets/StartScene/GameOver.cs
@@ -10,6 +10,15 @@ public class GameOver : MonoBehaviour
 
     void Update()
     {
-        gameOverText.text = "You Scored: " + ScoreManager.Instance.scoreText.text;
+        string stringToPrint = "";
+        stringToPrint += "Misdemeanors: \n" + NpcCopManager.Instance.copsKilled.ToString() + " Counts of Vandalism";
+        stringToPrint += "Felonies: \n" + NpcCopManager.Instance.civsKilled.ToString() + " Counts of Manslaughter";
+        stringToPrint += "\n" + NpcCopManager.Instance.carsStolen.ToString() + " Counts of Grand Theft Auto";
+        stringToPrint += "Capital Felonies: \n" + NpcCopManager.Instance.copsKilled.ToString() + " Counts of Police Murder";
+        stringToPrint += "\nYou Scored: " + ScoreManager.Instance.GetScore();
+
+
+
+         gameOverText.text = stringToPrint;
     }
 }
